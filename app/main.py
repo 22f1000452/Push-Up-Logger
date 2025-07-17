07b225afb5c31225b4e0b8abbb2f5169ecc24c39
Mysterious-Soul <22f1000452@student.onlinedegree.iitm.ac.in>
2025-07-17 21:55:42 +0530

    almost CRUD operation are done

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required, current_user
+from datetime import datetime
+
+from app import db
+from app.models import User
+from app.models import Workout
 
 main = Blueprint('main', __name__)
 
@@ -11,3 +16,69 @@ def index():
 @login_required
 def profile():
   return render_template('profile.html', name=current_user.name)
+
+@main.route('/workouts')
+@login_required
+def workouts():
+  workouts = Workout.query.filter_by(user_id=current_user.id).all()
+  return render_template('workouts.html', workouts=workouts)
+
+@main.route('/workout/new')
+@login_required
+def new_workout():
+  return render_template('new_workout.html')
+
+@main.route('/workout/new', methods=['POST'])
+@login_required
+def new_workout_post():
+  pushup = int(request.form.get('pushups'))
+  comment = request.form.get('comments')
+  date = request.form.get('date')
+  workout = Workout(user_id=current_user.id, date=datetime.strptime(date, '%Y-%m-%d %H:%M:%S'), pushups=pushup, comments=comment, user=current_user)
+  db.session.add(workout)
+  db.session.commit()
+
+  flash('Workout added successfully!', 'success')
+  return redirect(url_for('main.workouts'))
+
+@main.route('/workout/update/<int:workout_id>')
+@login_required
+def update_workout(workout_id):
+  workout = Workout.query.get_or_404(workout_id)
+  # if workout.user_id != current_user.id:
+  #   flash('You do not have permission to edit this workout.', 'danger')
+  #   return redirect(url_for('main.workouts'))
+
+  return render_template('update_workout.html', workout=workout)
+
+@main.route('/workout/update/<int:workout_id>', methods=['POST'])
+@login_required
+def update_workout_post(workout_id):
+  workout = Workout.query.get_or_404(workout_id)
+  # if workout.user_id != current_user.id:
+  #   flash('You do not have permission to edit this workout.', 'danger')
+  #   return redirect(url_for('main.workouts'))
+
+  workout.pushups = int(request.form.get('pushups'))
+  workout.comments = request.form.get('comments')
+  workout.date = datetime.strptime(request.form.get('date'), '%Y-%m-%d %H:%M:%S')
+
+  db.session.commit()
+
+  flash('Workout updated successfully!', 'success')
+  return redirect(url_for('main.workouts'))
+
+@main.route('/workout/delete/<int:workout_id>')
+@login_required
+def delete_workout(workout_id):
+  workout = Workout.query.get_or_404(workout_id)
+  if workout.user_id != current_user.id:
+    flash('You do not have permission to delete this workout.', 'danger')
+    return redirect(url_for('main.workouts'))
+
+  db.session.delete(workout)
+  db.session.commit()
+
+  flash('Workout deleted successfully!', 'success')
+  return redirect(url_for('main.workouts'))
+
--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
 from app import db
 from flask_login import UserMixin
+from datetime import datetime
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(200), nullable=False)
+
+class Workout(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  date = db.Column(db.DateTime, nullable=False)
+  pushups = db.Column(db.Integer, nullable=False)
+  comments = db.Column(db.String(200), nullable=True)
+  user = db.relationship('User', backref=db.backref('workouts', lazy=True))
